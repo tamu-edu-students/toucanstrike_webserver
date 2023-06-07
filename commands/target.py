e5252396819a7fd4ec77bc0721068cc20651ae6a
Snehith Bikumandla <snehithbikumandla@Snehiths-MacBook-Air.local>
2023-06-07 11:39:18 -0500

    indentation fixes

--- commands/target.py
+++ commands/target.py
@@ -10,59 +10,59 @@ from state import global_state
 
 
 def get_target_parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('target',
-						help='target for the attacks. Examples already implemented: malconv, gbdt_ember, sorel_dnn')
-	parser.add_argument('--model_path',
-						help='path to the weights of the model. Leave empty for MalConv to load default weights embedded in library.')
-	return parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('target',
+                        help='target for the attacks. Examples already implemented: malconv, gbdt_ember, sorel_dnn')
+    parser.add_argument('--model_path',
+                        help='path to the weights of the model. Leave empty for MalConv to load default weights embedded in library.')
+    return parser
 
 
 def target(args):
-	if args.target is None:
-		error_prompt('You have to set a target.')
-		error_prompt(f'Chose one from this list: {ALL_MODELS}')
-		return
+    if args.target is None:
+        error_prompt('You have to set a target.')
+        error_prompt(f'Chose one from this list: {ALL_MODELS}')
+        return
 
-	if args.target == MALCONV:
-		clf = CClassifierEnd2EndMalware(model=MalConv())
-		if args.model_path is None:
-			clf.load_pretrained_model()
-		else:
-			clf.load_pretrained_model(args.model_path)
-		_set_target(clf)
-		return
+    if args.target == MALCONV:
+        clf = CClassifierEnd2EndMalware(model=MalConv())
+        if args.model_path is None:
+            clf.load_pretrained_model()
+        else:
+            clf.load_pretrained_model(args.model_path)
+        _set_target(clf)
+        return
 
-	if not os.path.isdir(global_state.plugin_path):
-		crash_prompt(f"The plugin path {global_state.plugin_path} does not exists!")
-		return
+    if not os.path.isdir(global_state.plugin_path):
+        crash_prompt(f"The plugin path {global_state.plugin_path} does not exists!")
+        return
 
-	plugins = os.listdir(global_state.plugin_path)
-	if args.target not in plugins:
-		error_prompt(f"The target {args.target} does not exists among the plugins.")
-		return
+    plugins = os.listdir(global_state.plugin_path)
+    if args.target not in plugins:
+        error_prompt(f"The target {args.target} does not exists among the plugins.")
+        return
 
-	try:
-		module = importlib.import_module(f'plugins.{args.target}.model')
-		clf = module.load_model(args.model_path)
+    try:
+        module = importlib.import_module(f'plugins.{args.target}.model')
+        clf = module.load_model(args.model_path)
 
-		# # Train the model with the given train data file
+        # # Train the model with the given train data file
         if args.train_data is not None:
             train_data = load_train_data(args.train_data)
             # Train the model with the train data
             clf.fit(train_data.features, train_data.labels)
-
-		_set_target(clf)
-		return
-	except Exception as e:
-		crash_prompt(f"Can't import plugin {args.target}")
-		crash_prompt(f"Error was: {e}")
-		return
+            
+        _set_target(clf)
+        return
+    except Exception as e:
+        crash_prompt(f"Can't import plugin {args.target}")
+        crash_prompt(f"Error was: {e}")
+        return
 
 
 def _set_target(clf):
-	global_state.target = clf
-	success_prompt('Target set!')
+    global_state.target = clf
+    success_prompt('Target set!')
 
 def load_train_data(file_path):
     # Assuming the train data file is a CSV file where each row represents a sample,

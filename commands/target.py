c06ffcfb2bf1ad29fd64ef43372ef55962f854b7
snehith57624 <snehithb57624@gmail.com>
2023-06-18 01:53:47 -0500

    training with basic data

--- commands/target.py
+++ commands/target.py
@@ -2,11 +2,19 @@ import argparse
 import importlib.util
 import os
 
-from secml_malware.models import CClassifierEnd2EndMalware, MalConv
+import numpy as np
+from secml_malware.models import CClassifierEnd2EndMalware, MalConv, End2EndModel
 
 from constants import *
 from prompts import error_prompt, success_prompt, crash_prompt
 from state import global_state
+
+import torch
+import torch.nn as nn
+
+from libauc.optimizers import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from sklearn.metrics import f1_score
 
 
 def get_target_parser():
@@ -29,9 +37,9 @@ def target(args):
 
         model = model.train(True)
         # print("ok step1")
-        # model = train_model(model, ['//toucanstrike_webserver/static/Rbot-O.7z'], [1.0])
+        model = train_model(model, ['//toucanstrike_webserver/static/Rbot-O.7z'], [1.0])
         # print("ok step2")
-        global_state.model = model
+        # global_state.model = model
         clf = CClassifierEnd2EndMalware(model=model)
         if args.model_path is None:
             clf.load_pretrained_model()
@@ -65,3 +73,102 @@ def target(args):
 def _set_target(clf):
     global_state.target = clf
     success_prompt('Target set!')
+
+
+# def train_model(model, label):
+#     # num_classes = 1
+#     try:
+#         model.train(True)
+#         criterion = nn.BCELoss()
+#         optimizer = Adam(model.parameters(), lr=0.01)
+#         scheduler = ReduceLROnPlateau(optimizer, patience=3, verbose=True, factor=0.5,
+#                                       threshold=0.001, min_lr=0.00001, mode='max')
+#         net: CClassifierEnd2EndMalware = global_state.target
+#         print("ok inside train 3")
+#         loss = 0
+#         epochs = 10
+#         y_pred = 0
+#         for file_path in global_state.data_paths:
+#             with open(file_path, 'rb') as handle:
+#                 bytecode = handle.read()
+#         for epoch in range(epochs):
+#             x1 = End2EndModel.bytes_to_numpy(bytecode, net.get_input_max_length(), net.get_embedding_value(),
+#                                              net.get_is_shifting_values())
+#             x = CArray(x1).atleast_2d()
+#             print("check ", torch.from_numpy(x).shape)
+#             y_pred = model.embedd_and_forward(model.embed(x))
+#             print("y_pred ", y_pred)
+#             # y_pred, adv_score, adv_ds, f_obj = attack.run(x, y)
+#             y_pred = [(y_pred.item()) * 1.0]
+#             print("differnce ", y_pred, label)
+#             loss = criterion(torch.tensor(y_pred), torch.tensor(label))
+#             print("ok inside train 4")
+#             loss.requires_grad = True
+#             optimizer.zero_grad()
+#             print("ok inside train 5")
+#             loss.backward()
+#             print("ok inside train 6")
+#             optimizer.step()
+#             net = CClassifierEnd2EndMalware(model)
+#         print("ok inside train 7")
+#         model.eval()
+#         print("ok inside train 8")
+#         scheduler.step(f1_score(y_pred, label))
+#         print("ok inside train 4")
+#         print(loss)
+#     except Exception as e:
+#         print(e)
+#     return model
+
+
+def train_model(model, path, label):
+    print("ok inside train 1")
+    try:
+        print("global_state.data_paths ", global_state.data_paths)
+        for file_path in path:
+            with open(file_path, 'rb') as handle:
+                bytecode = handle.read()
+            print("ok inside train 2")
+            print("global_state.target ", global_state.target)
+            net: CClassifierEnd2EndMalware = global_state.target
+            x = End2EndModel.bytes_to_numpy(bytecode, net.get_input_max_length(), net.get_embedding_value(),
+                                            net.get_is_shifting_values())
+
+            model = MalConv()
+            x = np.expand_dims(x, axis=0)
+            x = torch.from_numpy(x)
+            model.train(True)
+            criterion = nn.BCELoss()
+            optimizer = Adam(model.parameters(), lr=0.01)
+            scheduler = ReduceLROnPlateau(optimizer, patience=3, verbose=True, factor=0.5,
+                                          threshold=0.001, min_lr=0.00001, mode='max')
+            print("ok inside train 3")
+            epochs = 3
+            y_pred = 0
+            for epoch in range(epochs):
+                print("ok inside train 4 ", model.embed(x))
+                y_pred = model.embedd_and_forward(model.embed(x))
+                print("ok inside train 5")
+                # y_pred = y_pred
+                label = torch.tensor(label)  # Convert label to a PyTorch tensor
+
+                # Reshape label to match the shape of y_pred
+                label = label.view(y_pred.shape)
+                print("ok inside train 6 ", y_pred)
+                print("ok inside train 7 ", label)
+                loss = criterion(y_pred, label)
+                print("ok inside train 8 ", loss)
+                optimizer.zero_grad()
+                print("ok inside train 9")
+                loss.backward()
+                print("ok inside train 10")
+                optimizer.step()
+                print("ok inside train 11")
+            model.eval()
+            print("ok inside train 12")
+            f1 = f1_score(label.detach().numpy(), y_pred.detach().numpy())
+            scheduler.step(f1)
+            print("ok inside train 13")
+    except Exception as e:
+        print(e)
+    return model
